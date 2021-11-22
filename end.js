9b6db5c0a4723802ea6707d6fbd7691f0cc57fcf
Ayoub Mabrouk <ayoubmabroukcontact@gmail.com>
2021-11-22 15:33:13 +0100

    adding json file

--- end.js
+++ end.js
@@ -23,9 +23,8 @@ saveHighScore = e => {
 
     highScores.push(score)
 
-    highScores.sort((a,b) => {
-        return b.score - a.score
-    })
+    highScores.sort((a,b) =>b.score - a.score
+    )
 
     highScores.splice(5)
 
