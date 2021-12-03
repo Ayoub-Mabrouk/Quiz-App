2113a1aac1971896791791cc909d60a4c5e48e6d
Ayoub Mabrouk <77799760+Ayoub-Mabrouk@users.noreply.github.com>
2021-12-03 09:39:40 +0100

    adding answers

--- classes.js
+++ classes.js
@@ -1,82 +1,107 @@
 class User {
-    static static_id = 0;
-    constructor(first_name, last_name, email) {
-      this.first_name = first_name;
-      this.last_name = last_name;
-      this.email = email;
-      this.id = `${[...this.constructor.name].shift()}${[
-        ...this.constructor.name,
-      ].pop()}${++User.static_id}${Date.now()}${Math.floor(
-        Math.random() * 100
-      )}${Math.floor(Math.random() * 100)}`;
-    }
-    tostring() {
-      return Object.entries(this).map((e) => `the ${e[0]} is ${e[1]}`).join`, `;
-    }
+  static static_id = 0;
+  constructor(first_name, last_name, email) {
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.id = `${[...this.constructor.name].shift()}${[
+      ...this.constructor.name,
+    ].pop()}${++User.static_id}${Date.now()}${Math.floor(
+      Math.random() * 100
+    )}${Math.floor(Math.random() * 100)}`;
   }
-  
-  class Teacher extends User {
-      constructor({first_name, last_name, email, profession}) {
-        super(first_name, last_name, email);
-        this.profession = profession;
-      }
-    }
-  class Student extends User {
-    constructor(first_name, last_name, email) {
-      super(first_name, last_name, email);
-    }
+  tostring() {
+    return Object.entries(this).map((e) => `the ${e[0]} is ${e[1]}`).join`, `;
   }
-  class Subject {
-    static static_id = 0;
-    constructor(name, type, children) {
-      this.id = `${[...this.constructor.name].shift()}${[
-        ...this.constructor.name,
-      ].pop()}${++Subject.static_id}${Date.now()}${Math.floor(
-        Math.random() * 100
-      )}${Math.floor(Math.random() * 100)}`;
-      this.name = name;
-      this.type = type;
-      this.children = JSON.parse(JSON.stringify(children));
-    }
-  }
-  class Question {
-    static static_id = 0;
-    constructor({text, answers=[], points, level}) {
-      this.id = `${[...this.constructor.name].shift()}${[
-        ...this.constructor.name,
-      ].pop()}${++Question.static_id}${Date.now()}${Math.floor(
-        Math.random() * 100
-      )}${Math.floor(Math.random() * 100)}`;
-      this.text = text;
-      this.points = points;
-      this.answers = answers;
-      this.level = level;
-    }
-  }
-  class Answer {
-    static static_id = 0;
-    constructor(text, correct) {
-      this.id = `${[...this.constructor.name].shift()}${[
-        ...this.constructor.name,
-      ].pop()}${++Answer.static_id}${Date.now()}${Math.floor(
-        Math.random() * 100
-      )}${Math.floor(Math.random() * 100)}`;
-      this.text = text;
-      this.correct = correct;
-    }
-  }
-  
-  class Level {
-      static static_id = 0;
-      constructor(text, min_points,max_points) {
-        this.id = `${[...this.constructor.name].shift()}${[
-          ...this.constructor.name,
-        ].pop()}${++Level.static_id}${Date.now()}${Math.floor(
-          Math.random() * 100
-        )}${Math.floor(Math.random() * 100)}`;
-        this.min_points = min_points;
-        this.max_points = max_points;
-      }
-    }
+}
 
-    export  {User,Teacher,Student,Subject,Question,Answer,Level};
+class Teacher extends User {
+  constructor({ first_name, last_name, email, profession }) {
+    super(first_name, last_name, email);
+    this.profession = profession;
+  }
+  async add_question(question) {
+    
+    let questionInstance=new Question(question);
+    let response = await fetch("http://localhost:3000/Questions", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify(questionInstance),
+    });
+    let q = await response.json();
+    return q;
+  }
+}
+class Student extends User {
+  constructor(first_name, last_name, email) {
+    super(first_name, last_name, email);
+  }
+}
+class Subject {
+  static static_id = 0;
+  constructor(name, type, children) {
+    this.id = `${[...this.constructor.name].shift()}${[
+      ...this.constructor.name,
+    ].pop()}${++Subject.static_id}${Date.now()}${Math.floor(
+      Math.random() * 100
+    )}${Math.floor(Math.random() * 100)}`;
+    this.name = name;
+    this.type = type;
+    this.children = JSON.parse(JSON.stringify(children));
+  }
+
+  static async get_all_subjects() {
+    const response = await fetch("http://localhost:3000/Subjects");
+    let Subjects = await response.json();
+    return Subjects;
+  }
+}
+class Question {
+  static static_id = 0;
+  constructor({ text, answers = [], points, subjectID, level }) {
+    this.id = `${[...this.constructor.name].shift()}${[
+      ...this.constructor.name,
+    ].pop()}${++Question.static_id}${Date.now()}${Math.floor(
+      Math.random() * 100
+    )}${Math.floor(Math.random() * 100)}`;
+    this.text = text;
+    this.points = points;
+    this.answers = answers;
+    this.subjectID = subjectID;
+    this.level = level;
+  }
+}
+class Answer {
+  static static_id = 0;
+  constructor(text, correct) {
+    this.id = `${[...this.constructor.name].shift()}${[
+      ...this.constructor.name,
+    ].pop()}${++Answer.static_id}${Date.now()}${Math.floor(
+      Math.random() * 100
+    )}${Math.floor(Math.random() * 100)}`;
+    this.text = text;
+    this.correct = correct;
+  }
+}
+
+class Level {
+  static static_id = 0;
+  constructor(min_points, max_points) {
+    this.id = `${[...this.constructor.name].shift()}${[
+      ...this.constructor.name,
+    ].pop()}${++Level.static_id}${Date.now()}${Math.floor(
+      Math.random() * 100
+    )}${Math.floor(Math.random() * 100)}`;
+    this.min_points = min_points;
+    this.max_points = max_points;
+  }
+  static async get_all_levels() {
+    const response = await fetch("http://localhost:3000/Levels");
+    let Subjects = await response.json();
+    return Subjects;
+  }
+}
+
+export { User, Teacher, Student, Subject, Question, Answer, Level };
--- dashboard.js
+++ dashboard.js
@@ -21,7 +21,8 @@ let questions_link = document.querySelector("#questions_link");
 let subjects_link = document.querySelector("#subjects_link");
 let tests_link = document.querySelector("#tests_link");
 teacher_link.addEventListener("click", show_teacher_tab);
-
+questions_link.addEventListener("click", show_question_tab);
+answers_link.addEventListener("click", show_answer_tab);
 
 
 let Teachers=[];
@@ -29,7 +30,6 @@ let Teachers=[];
 async function Get_all_teachers() {
   const response = await fetch("http://localhost:3000/Teachers");
   Teachers = await response.json();
-  console.log(Teachers);
 }
 
 async function create_teachers_table() {
@@ -80,27 +80,7 @@ async function create_teachers_table() {
       block
       md:table-cell
     ">
-  Math
-</td>
-<td class="
-      p-2
-      md:border md:border-grey-500
-      text-left
-      block
-      md:table-cell
-    ">
-  <button class="
-        bg-red-500
-        hover:bg-red-700
-        text-white
-        font-bold
-        py-1
-        px-2
-        border border-red-500
-        rounded
-      ">
-    Delete
-  </button>
+  ${e.profession}
 </td>
 </tr>`)
   );
@@ -245,19 +225,6 @@ async function show_teacher_tab() {
             ">
           Profession
         </th>
-        <th class="
-              bg-red-500
-              p-2
-              text-white
-              font-bold
-              md:border md:border-grey-500
-              text-left
-              block
-              md:table-cell
-              rounded-tr-lg
-            ">
-          Manage
-        </th>
       </tr>
     </thead>
     <tbody class="block md:table-row-group" id="tbody">
@@ -279,7 +246,6 @@ async function show_teacher_tab() {
         body: JSON.stringify(new Teacher({...data})),
       });
       show_teacher_tab();
-      console.log(response.json());
     } else {
       console.log("fill all the teacher's input");
     }
@@ -296,263 +262,340 @@ show_teacher_tab();
 //
 //
 //
-// let Questions=[]
-// async function get_all_questions() {
-//   const response = await fetch("http://localhost:3000/Quetions");
-//   Questions = await response.json();
-//   console.log(Questions);
-// }
 
-// async function create_questions_table() {
-//   Questions=[];
-//   await get_all_questions();
-//   //get the table container to start modifiying it
-//   Questions.map((e,i)=>
-//   tbody.insertAdjacentHTML(
-//     "beforeend",
-//     `<tr class="
-//     bg-gray-300
-//     border border-grey-500
-//     md:border-none
-//     block
-//     md:table-row
-//   ">
-// <td class="
-//       p-2
-//       md:border md:border-grey-500
-//       text-left
-//       block
-//       md:table-cell
-//     ">
-//   ${e.first_name}
-// </td>
-// <td class="
-//       p-2
-//       md:border md:border-grey-500
-//       text-left
-//       block
-//       md:table-cell
-//     ">
-//     ${e.last_name}
-// </td>
-// <td class="
-//       p-2
-//       md:border md:border-grey-500
-//       text-left
-//       block
-//       md:table-cell
-//     ">
-//     ${e.email}
-// </td>
-// <td class="
-//       p-2
-//       md:border md:border-grey-500
-//       text-left
-//       block
-//       md:table-cell
-//     ">
-//   Math
-// </td>
-// <td class="
-//       p-2
-//       md:border md:border-grey-500
-//       text-left
-//       block
-//       md:table-cell
-//     ">
-//   <button class="
-//         bg-red-500
-//         hover:bg-red-700
-//         text-white
-//         font-bold
-//         py-1
-//         px-2
-//         border border-red-500
-//         rounded
-//       ">
-//     Delete
-//   </button>
-// </td>
-// </tr>`)
-//   );
-// }
-// async function show_teacher_tab() {
-//   content_container.innerHTML=` <form action="" id="form_add_question" class="md:w-1/2 w-full">
-//   <div class="shadow-xl">
-//     <div class="
-//           border-b border-gray-300
-//           flex
-//           items-center
-//           bg-red-500
-//           rounded-t-lg
-//         ">
-//       <label class="text-red-100 w-24 text-right ml-4 mr-8" for="name">First Name</label>
-//       <input class="
-//             flex-1
-//             p-4
-//             pl-0
-//             bg-transparent
-//             placeholder-gray-300
-//             outline-none
-//             text-white
-//           " type="text" id="name" name="first_name" placeholder="your first name" />
-//     </div>
+let Questions=[]
+async function get_all_questions() {
+  const response = await fetch("http://localhost:3000/Questions");
+  Questions = await response.json();
+}
 
-//     <div class="border-b border-gray-300 flex items-center bg-red-500">
-//       <label class="text-red-100 w-24 text-right ml-4 mr-8" for="last_name">Last Name</label>
-//       <input class="
-//             flex-1
-//             p-4
-//             pl-0
-//             bg-transparent
-//             placeholder-gray-300
-//             outline-none
-//             text-white
-//           " type="text" id="last_name" name="last_name" placeholder="your last name" />
-//     </div>
+async function create_questions_table() {
+  Questions=[];
+  let levels = await Level.get_all_levels();
+  let subjects = await Subject.get_all_subjects();
+  await get_all_questions();
+  //get the table container to start modifiying it
+  Questions.map((e,i)=>
+  tbody.insertAdjacentHTML(
+    "beforeend",
+    `<tr class="
+    bg-gray-300
+    border border-grey-500
+    md:border-none
+    block
+    md:table-row
+  ">
+<td class="
+      p-2
+      md:border md:border-grey-500
+      text-left
+      block
+      md:table-cell
+    ">
+  ${subjects.filter(sub=>sub.id==e.subjectID)[0].text}
+</td>
+<td class="
+      p-2
+      md:border md:border-grey-500
+      text-left
+      block
+      md:table-cell
+    ">
+  ${e.text}
+</td>
+<td class="
+      p-2
+      md:border md:border-grey-500
+      text-left
+      block
+      md:table-cell
+    ">
+    ${e.points}
+</td>
+<td class="
+      p-2
+      md:border md:border-grey-500
+      text-left
+      block
+      md:table-cell
+    ">
+    ${levels.filter(level=>level.id==e.level)[0].min_points}-${levels.filter(level=>level.id==e.level)[0].max_points}
+</td>
+</tr>`)
+  );
+}
+async function show_question_tab() {
+  
+  let subjects = await Subject.get_all_subjects();
+  let levels = await Level.get_all_levels();
 
-//     <div class="border-b border-gray-300 flex items-center bg-red-500">
-//       <label class="text-red-100 w-24 text-right ml-4 mr-8" for="profession">Profession</label>
-//       <input class="
-//             flex-1
-//             p-4
-//             pl-0
-//             bg-transparent
-//             placeholder-gray-300
-//             outline-none
-//             text-white
-//           " type="text" id="profession" name="profession" placeholder="your profession" />
-//     </div>
+  content_container.innerHTML=`<form action="" id="form_add_question" class="md:w-1/2 w-full">
+  <div class="shadow-xl">
+    <div class="
+          border-b border-gray-300
+          flex
+          items-center
+          bg-red-500
+          rounded-t-lg
+        ">
+      <label class="text-red-100 w-24 text-right ml-4 mr-8" for="name">Question</label>
+      <input class="
+            flex-1
+            p-4
+            pl-0
+            bg-transparent
+            placeholder-gray-300
+            outline-none
+            text-white
+          " type="text" id="name" name="text" placeholder="the question" />
+    </div>
 
-//     <div class="flex items-center bg-red-500 rounded-b-lg mb-10">
-//       <label class="text-red-100 w-24 text-right ml-4 mr-8" for="email">Email</label>
-//       <input class="
-//             flex-1
-//             p-4
-//             pl-0
-//             bg-transparent
-//             placeholder-gray-300
-//             outline-none
-//             text-white
-//           " type="email" id="email" name="email" placeholder="your email" />
-//     </div>
-//   </div>
-//   <div class="flex">
-//     <button type="submit" class="
-//           mx-auto
-//           w-full
-//           bg-red-500
-//           p-3
-//           rounded
-//           text-red-100
-//           font-bold
-//           shadow
-//           hover:bg-red-700
-//         ">
-//       Add Teacher
-//     </button>
-//   </div>
-// </form>
-// <div class="teacher_table_container w-8/12">
-//   <table class="border-collapse block md:table shadow-xl w-full">
-//     <thead class="block md:table-header-group">
-//       <tr class="
-//             border border-grey-500
-//             md:border-none
-//             block
-//             md:table-row
-//             absolute
-//             -top-full
-//             md:top-auto
-//             -left-full
-//             md:left-auto md:relative
-//           ">
-//         <th class="
-//               bg-red-500
-//               p-2
-//               text-white
-//               font-bold
-//               md:border md:border-grey-500
-//               text-left
-//               block
-//               md:table-cell
-//               rounded-tl-lg
-//             ">
-//           First Name
-//         </th>
-//         <th class="
-//               bg-red-500
-//               p-2
-//               text-white
-//               font-bold
-//               md:border md:border-grey-500
-//               text-left
-//               block
-//               md:table-cell
-//             ">
-//           Last Name
-//         </th>
-//         <th class="
-//               bg-red-500
-//               p-2
-//               text-white
-//               font-bold
-//               md:border md:border-grey-500
-//               text-left
-//               block
-//               md:table-cell
-//             ">
-//           Email
-//         </th>
-//         <th class="
-//               bg-red-500
-//               p-2
-//               text-white
-//               font-bold
-//               md:border md:border-grey-500
-//               text-left
-//               block
-//               md:table-cell
-//             ">
-//           Profession
-//         </th>
-//         <th class="
-//               bg-red-500
-//               p-2
-//               text-white
-//               font-bold
-//               md:border md:border-grey-500
-//               text-left
-//               block
-//               md:table-cell
-//               rounded-tr-lg
-//             ">
-//           Manage
-//         </th>
-//       </tr>
-//     </thead>
-//     <tbody class="block md:table-row-group" id="tbody">
+    <div class="border-b border-gray-300 flex items-center bg-red-500">
+      <label class="text-red-100 w-24 text-right ml-4 mr-8" for="last_name">Points</label>
+      <input class="
+            flex-1
+            p-4
+            pl-0
+            bg-transparent
+            placeholder-gray-300
+            outline-none
+            text-white
+          " type="number" value="" id="points" name="points" placeholder="Points" />
+    </div>
+
+
+
+<div class="flex relative w-full justify-between bg-red-500 items-center">
+    <label class="text-red-100 w-24 text-right ml-4 mr-8" for="last_name">Level</label>
+      <select name="level" class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+        <option value="" disabled selected>Please choose a level</option>
+        ${levels.map(e=>`<option value=${e.id}>${e.min_points}-${e.max_points}</option>`).join``}
+      </select>
+    <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+      <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+    </div>
+</div>
+
+
+
+<div class="flex relative w-full justify-between bg-red-500 items-center">
+    <label class="text-red-100 w-24 text-right ml-4 mr-8" for="last_name">Subject</label>
+      <select name="subjectID" class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+        <option value="" disabled selected>Please choose subject</option>
+        ${subjects.filter(e=>e.type=="parent")?.map(parent=>`<optgroup label=${parent.text}>${subjects.filter(sub=>sub.type=="" && parent.children.includes(sub.id))?.map(sub=>`<option value=${sub.id}>${sub.text}</option>`).join('')}</optgroup>`).join('')}       
+      </select>
+    <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+      <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+    </div>
+</div>
+
+
+
+
+    
+  </div>
+  <div class="flex mt-4">
+    <button type="submit" class="
+          mx-auto
+          w-full
+          bg-red-500
+          p-3
+          rounded
+          text-red-100
+          font-bold
+          shadow
+          hover:bg-red-700
+        ">
+      Add Question
+    </button>
+  </div>
+</form>
+<div class="question_table_container w-8/12">
+  <table class="border-collapse block md:table shadow-xl w-full">
+    <thead class="block md:table-header-group">
+      <tr class="
+            border border-grey-500
+            md:border-none
+            block
+            md:table-row
+            absolute
+            -top-full
+            md:top-auto
+            -left-full
+            md:left-auto md:relative
+          ">
+        <th class="
+              bg-red-500
+              p-2
+              text-white
+              font-bold
+              md:border md:border-grey-500
+              text-left
+              block
+              md:table-cell
+              rounded-tl-lg
+            ">
+          Subject
+        </th>
+        <th class="
+              bg-red-500
+              p-2
+              text-white
+              font-bold
+              md:border md:border-grey-500
+              text-left
+              block
+              md:table-cell
+              rounded-tl-lg
+            ">
+          Question
+        </th>
+        <th class="
+              bg-red-500
+              p-2
+              text-white
+              font-bold
+              md:border md:border-grey-500
+              text-left
+              block
+              md:table-cell
+            ">
+          Points
+        </th>
+        <th class="
+              bg-red-500
+              p-2
+              text-white
+              font-bold
+              md:border md:border-grey-500
+              text-left
+              block
+              md:table-cell
+            ">
+          Level
+        </th>
+      </tr>
+    </thead>
+    <tbody class="block md:table-row-group" id="tbody">
       
-//     </tbody>
-//   </table>
-// </div>`
+    </tbody>
+  </table>
+</div>`
 
-//   await create_teachers_table();
-//   form_add_teacher.onsubmit = async (e) => {
-//     e.preventDefault();
-//     let data = Object.fromEntries(new FormData(e.target));
-//     if(Object.entries(data).every((e) => e[1].length > 0)) {
-//       let response = await fetch("http://localhost:3000/Questions", {
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         method: "POST",
-//         body: JSON.stringify(new Question({...data})),
-//       });
-//       show_teacher_tab();
-//       console.log(response.json());
-//     } else {
-//       console.log("fill all the teacher's input");
-//     }
-//   }}
 
+  await create_questions_table();
+  form_add_question.onsubmit = async (e) => {
+    e.preventDefault();
+    let data = Object.fromEntries(new FormData(e.target));
+    let min=levels.filter(level=>level.id==data.level)[0].min_points;
+    let max=levels.filter(level=>level.id==data.level)[0].max_points;
+    if(data.points<min || data.points>max){
+      console.log("choose points from the interval");
+      return;
+    }
+    let T= new Teacher({});
+    T.add_question(data)
+    show_question_tab();
+  }}
+
+//
+//
+//answers
+//
+//
+
+
+async function show_answer_tab() {
+  content_container.innerHTML=` <form action="" id="form_add_answer" class="md:w-1/2 w-full">
+  <div class="shadow-xl">
+    <div class="
+          border-b border-gray-300
+          flex
+          items-center
+          bg-red-500
+          rounded-t-lg
+        ">
+      <label class="text-red-100 w-24 text-right ml-4 mr-8" for="name">First Name</label>
+      <input class="
+            flex-1
+            p-4
+            pl-0
+            bg-transparent
+            placeholder-gray-300
+            outline-none
+            text-white
+          " type="text" id="name" name="first_name" placeholder="your first name" />
+    </div>
+
+    <div class="border-b border-gray-300 flex items-center bg-red-500">
+      <label class="text-red-100 w-24 text-right ml-4 mr-8" for="last_name">Last Name</label>
+      <input class="
+            flex-1
+            p-4
+            pl-0
+            bg-transparent
+            placeholder-gray-300
+            outline-none
+            text-white
+          " type="text" id="last_name" name="last_name" placeholder="your last name" />
+    </div>
+
+    <div class="border-b border-gray-300 flex items-center bg-red-500">
+      <label class="text-red-100 w-24 text-right ml-4 mr-8" for="profession">Profession</label>
+      <input class="
+            flex-1
+            p-4
+            pl-0
+            bg-transparent
+            placeholder-gray-300
+            outline-none
+            text-white
+          " type="text" id="profession" name="profession" placeholder="your profession" />
+    </div>
+
+    <div class="flex items-center bg-red-500 rounded-b-lg mb-10">
+      <label class="text-red-100 w-24 text-right ml-4 mr-8" for="email">Email</label>
+      <input class="
+            flex-1
+            p-4
+            pl-0
+            bg-transparent
+            placeholder-gray-300
+            outline-none
+            text-white
+          " type="email" id="email" name="email" placeholder="your email" />
+    </div>
+  </div>
+  <div class="flex">
+    <button type="submit" class="
+          mx-auto
+          w-full
+          bg-red-500
+          p-3
+          rounded
+          text-red-100
+          font-bold
+          shadow
+          hover:bg-red-700
+        ">
+      Add Teacher
+    </button>
+  </div>
+</form>`
+
+  form_add_answer.onsubmit = async (e) => {
+    e.preventDefault();
+    let data = Object.fromEntries(new FormData(e.target));
+    
+      let response = await fetch("http://localhost:3000/Answers", {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+        body: JSON.stringify(new Teacher({...data})),
+      });
+      show_answer_tab();
+    
+  };
+  
+}
